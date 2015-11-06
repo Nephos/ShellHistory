bbea588a5e8c826533bf3275bdbb8d1bcb54bbfc
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-11-06 21:20:23 +0100

    update

--- app/force_utf-8.rb
+++ app/force_utf-8.rb
@@ -1,6 +1,6 @@
 class String
 
-  # from https://github.com/pouleta/rubyhelper/
+  # from https://github.com/Nephos/rubyhelper/
   def force_utf8 replace='?'
     raise ArgumentError, 'replace is not a valid char (String)' unless replace.is_a? String
     return self.encode('UTF-8', invalid: :replace, undef: :replace, replace: replace)
